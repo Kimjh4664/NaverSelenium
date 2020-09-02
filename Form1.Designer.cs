7139b4eb70a468ba705c160343892f032066b9ec
김정훈 <mr1or@naver.com>
2020-09-02 11:44:49 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace NaverSeleniumHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.urlInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.idInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pageNumberInput = new System.Windows.Forms.TextBox();
+            this.userMessage = new System.Windows.Forms.RichTextBox();
+            this.userIdInput = new System.Windows.Forms.TextBox();
+            this.userPwInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dir = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(65, 351);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(290, 62);
+            this.loginBtn.TabIndex = 2;
+            this.loginBtn.Text = "동작개시";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(65, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(290, 62);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "로그인페이지";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 423);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Created By 뚜따뛰";
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(62, 116);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(27, 18);
+            this.urlLabel.TabIndex = 5;
+            this.urlLabel.Text = "url";
+            // 
+            // urlInput
+            // 
+            this.urlInput.Location = new System.Drawing.Point(65, 137);
+            this.urlInput.Name = "urlInput";
+            this.urlInput.Size = new System.Drawing.Size(290, 28);
+            this.urlInput.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "가입인사ID";
+            // 
+            // idInput
+            // 
+            this.idInput.Location = new System.Drawing.Point(65, 209);
+            this.idInput.Name = "idInput";
+            this.idInput.Size = new System.Drawing.Size(290, 28);
+            this.idInput.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "서웃추 메시지";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(373, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 18);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "진행상황";
+            // 
+            // pageNumberInput
+            // 
+            this.pageNumberInput.Location = new System.Drawing.Point(376, 61);
+            this.pageNumberInput.Name = "pageNumberInput";
+            this.pageNumberInput.Size = new System.Drawing.Size(77, 28);
+            this.pageNumberInput.TabIndex = 12;
+            // 
+            // userMessage
+            // 
+            this.userMessage.Location = new System.Drawing.Point(65, 290);
+            this.userMessage.Name = "userMessage";
+            this.userMessage.Size = new System.Drawing.Size(290, 52);
+            this.userMessage.TabIndex = 13;
+            this.userMessage.Text = "";
+            // 
+            // userIdInput
+            // 
+            this.userIdInput.Location = new System.Drawing.Point(522, 27);
+            this.userIdInput.Name = "userIdInput";
+            this.userIdInput.Size = new System.Drawing.Size(100, 28);
+            this.userIdInput.TabIndex = 14;
+            // 
+            // userPwInput
+            // 
+            this.userPwInput.Location = new System.Drawing.Point(522, 68);
+            this.userPwInput.Name = "userPwInput";
+            this.userPwInput.PasswordChar = '*';
+            this.userPwInput.Size = new System.Drawing.Size(100, 28);
+            this.userPwInput.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(522, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 34);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "로그인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // dir
+            // 
+            this.dir.Location = new System.Drawing.Point(402, 185);
+            this.dir.Name = "dir";
+            this.dir.Size = new System.Drawing.Size(231, 28);
+            this.dir.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(399, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 18);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "경로";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(402, 351);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(231, 62);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "중지";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dir);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.userPwInput);
+            this.Controls.Add(this.userIdInput);
+            this.Controls.Add(this.userMessage);
+            this.Controls.Add(this.pageNumberInput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.idInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.urlInput);
+            this.Controls.Add(this.urlLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.loginBtn);
+            this.Name = "Form1";
+            this.Text = "뚜따뛰";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.TextBox urlInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox idInput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox pageNumberInput;
+        private System.Windows.Forms.RichTextBox userMessage;
+        private System.Windows.Forms.TextBox userIdInput;
+        private System.Windows.Forms.TextBox userPwInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox dir;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,307 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace NaverSeleniumHelper
+{
+    public partial class Form1 : Form
+    {
+        protected ChromeDriverService _driverService = null;
+        protected ChromeOptions _options = null;
+        protected ChromeDriver _driver = null;
+        private string userEmail = String.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+            _driverService = ChromeDriverService.CreateDefaultService();
+            _driverService.HideCommandPromptWindow = true;
+
+            _options = new ChromeOptions();
+            _options.AddArgument("disable-gpu");
+            pageNumberInput.Text = "1";
+            dir.Text = @"C:\";
+        }
+        private void SaveTxtFile(string email)
+        {
+            try
+            {
+                email = email + "@naver.com";
+                if (userEmail == "")
+                {
+                    userEmail = email;
+                }
+                else
+                {
+                    userEmail = userEmail + "," + email;
+                }
+                // 파일 경로
+                string path = dir.Text;
+                // 경로를 지정하지 않은경우
+                if (path == "")
+                {
+                    MessageBox.Show("경로를 입력해주세요.");
+                    // 종료
+                    return;
+                }
+
+                StreamWriter log;
+                FileStream fileStream = null;
+                DirectoryInfo logDirInfo = null;
+                FileInfo logFileInfo;
+
+                string txtFileDir = path + "list.txt";
+                logFileInfo = new FileInfo(txtFileDir);
+                logDirInfo = new DirectoryInfo(logFileInfo.DirectoryName);
+                if (!logDirInfo.Exists) logDirInfo.Create();
+                if (!logFileInfo.Exists)
+                {
+                    fileStream = logFileInfo.Create();
+                }
+                else
+                {
+                    fileStream = new FileStream(txtFileDir, FileMode.Append);
+                }
+                log = new StreamWriter(fileStream);
+                log.WriteLine(email);
+                log.Close();
+            }
+            catch(Exception ex)
+            {   
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // https://cafe.naver.com/fulpot
+            string url = urlInput.Text;
+            // "menuLink16"
+            string id = idInput.Text;
+            string inputtedPageNumber = pageNumberInput.Text;
+            if (url == "" || id == "")
+            {
+                MessageBox.Show("카페 URL이랑 가입인사 HTML ID 속성값을 입력해라");
+                return;
+            }
+            try
+            {
+                _driver.Navigate().GoToUrl(url);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("로그인페이지 버튼부터 눌러서 진행하세요^^,,");
+                return;
+            }
+
+            // 사용자 메세지가 존재하지 않을경우 
+            if (userMessage.Text == "")
+            {
+                MessageBox.Show("서웃추 메세지를 입력해야 보내죠 ㅡ ㅡ");
+                return;
+            }
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            var element = _driver.FindElementByXPath("//*[@id='" + id + "']");
+            element.Click();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+
+            _driver.SwitchTo().Frame("cafe_main");
+            Thread.Sleep(2000);
+            var page = Int32.Parse(inputtedPageNumber);
+            while (true)
+            {
+                var el = _driver.FindElements(By.CssSelector("div.article-board:not(#upperArticleList) .pers_nick_area a"));
+                var nav = _driver.FindElement(By.CssSelector("div.prev-next > a.on"));
+                // 게시판 링크 
+                var boardLink = nav.GetAttribute("href");
+                string firstWindow = _driver.CurrentWindowHandle;
+                var uri = new Uri(boardLink);
+                var searchPage = HttpUtility.ParseQueryString(uri.Query).Get("search.page");
+                var normalStrArr = boardLink.Split(new string[] { "&search.page" }, StringSplitOptions.None);
+                string pageUrl = normalStrArr[0] + "&search.page=" + page;
+
+                foreach (var key in _driver.WindowHandles)
+                {
+                    if (firstWindow != key)
+                    {
+                        // 처음으로 돌아감 
+                        _driver.SwitchTo().Window(key);
+                        _driver.Close();
+                    }
+                }
+                // 처음으로 돌아감 
+                _driver.SwitchTo().Window(firstWindow);
+                _driver.Navigate().GoToUrl(pageUrl);
+                _driver.SwitchTo().Frame("cafe_main");
+                el = _driver.FindElements(By.CssSelector("div.article-board:not(#upperArticleList) .pers_nick_area a"));
+                nav = _driver.FindElement(By.CssSelector("div.prev-next > a.on"));
+                if (el.Count == 0)
+                {
+                    MessageBox.Show("URL 이 잘못되어서 추적을 못했거나, 게시판이 최대 페이지까지 갔으므로 크롤링을 종료합니다.");
+                    return;
+                }
+                // 작성자 명 목록 
+                foreach (var item in el)
+                {
+                    try
+                    {
+                        // 작성자 명 클릭  
+                        item.Click();
+                        // 딜레이 
+                        Thread.Sleep(1000);
+                        // 블로그 보기 태그 링크 
+                        var clickToElement = _driver.FindElements(By.CssSelector(".perid-layer a"));
+
+                        // 블로그 보기 태그 클릭 
+                        if (clickToElement.Count == 0)
+                            continue;
+
+                        clickToElement[0].Click();
+                        Thread.Sleep(1000);
+                        _driver.SwitchTo().Window(_driver.WindowHandles.Last());
+                        Thread.Sleep(1000);
+                        _driver.SwitchTo().Frame("mainFrame");
+                        Thread.Sleep(1000);
+                        SaveTxtFile(new Uri(_driver.Url).LocalPath.Replace("/", ""));
+                        // 이웃추가하기 버튼 
+                        var btn = _driver.FindElements(By.CssSelector("#blog-profile .btn_area > a"));
+                        btn[0].Click();
+                        Thread.Sleep(1000);
+
+                        _driver.SwitchTo().Window(_driver.WindowHandles.Last());
+                        Thread.Sleep(5000);
+
+                        btn = _driver.FindElements(By.CssSelector("label[for='each_buddy_add']"));
+                        try
+                        {
+                            btn[0].Click();
+                            Thread.Sleep(1000);
+                        }
+                        catch (Exception ex)
+                        {
+                            foreach (var key in _driver.WindowHandles)
+                            {
+                                if (firstWindow != key)
+                                {
+                                    // 처음으로 돌아감 
+                                    _driver.SwitchTo().Window(key);
+                                    _driver.Close();
+                                }
+                            }
+                            // 처음으로 돌아감 
+                            _driver.SwitchTo().Window(firstWindow);
+                            _driver.SwitchTo().Frame("cafe_main");
+                            continue;
+                        }
+
+                        btn = _driver.FindElements(By.CssSelector("._buddyAddNext"));
+                        btn[0].Click();
+                        Thread.Sleep(1000);
+                        bool isErr = false;
+                        try
+                        {
+                            var txtbox = _driver.FindElement(By.Id("message"));
+                            txtbox.SendKeys(userMessage.Text);
+                            Thread.Sleep(1000);
+
+                            btn = _driver.FindElements(By.CssSelector("._addBothBuddy"));
+                            btn[0].Click();
+                            Thread.Sleep(1000);
+                        }
+                        catch (Exception ex) { isErr = true; }
+                        finally
+                        {
+                            if (_driver.WindowHandles.Count > 1)
+                            {
+                                _driver.SwitchTo().Window(_driver.WindowHandles[1]);
+                            }
+                            _driver.Close();
+                        }
+
+                        foreach (var key in _driver.WindowHandles)
+                        {
+                            if (firstWindow != key)
+                            {
+                                // 처음으로 돌아감 
+                                _driver.SwitchTo().Window(key);
+                                _driver.Close();
+                            }
+                        }
+                        // 처음으로 돌아감 
+                        _driver.SwitchTo().Window(firstWindow);
+                        _driver.SwitchTo().Frame("cafe_main");
+                    }
+                    catch (Exception ex)
+                    {
+                        foreach (var key in _driver.WindowHandles)
+                        {
+                            if (firstWindow != key)
+                            {
+                                // 처음으로 돌아감 
+                                _driver.SwitchTo().Window(key);
+                                _driver.Close();
+                            }
+                        }
+                        // 처음으로 돌아감 
+                        _driver.SwitchTo().Window(firstWindow);
+                        _driver.SwitchTo().Frame("cafe_main");
+                    }
+                }
+                page += 1;
+                pageNumberInput.Text = page.ToString();
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            _driver = new ChromeDriver(_driverService, _options);
+            _driver.Navigate().GoToUrl("https://nid.naver.com/nidlogin.login");
+        }
+
+        private void UserMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            _driver = new ChromeDriver(_driverService, _options);
+            _driver.Navigate().GoToUrl("https://zhukov.github.io/webogram/#/login");
+
+            IWebElement naverAccID = _driver.FindElement(By.CssSelector("[name=phone_number]"));
+            Clipboard.SetText(userIdInput.Text);
+            naverAccID.SendKeys(OpenQA.Selenium.Keys.LeftControl + "V");
+            naverAccID.SendKeys(OpenQA.Selenium.Keys.Enter);
+
+            //_driver = new ChromeDriver(_driverService, _options);
+            //_driver.Navigate().GoToUrl("https://nid.naver.com/nidlogin.login");
+
+            //// 네이버 아이디 입력
+            //IWebElement naverAccID = _driver.FindElement(By.CssSelector("[name=id]"));
+            //Clipboard.SetText(userIdInput.Text);
+            //naverAccID.SendKeys(OpenQA.Selenium.Keys.LeftControl + "V");
+
+            //// 네이버 암호 입력
+            //IWebElement naverAccPass = _driver.FindElement(By.CssSelector("[name=pw]"));
+            //Clipboard.SetText(userPwInput.Text);
+            //naverAccPass.SendKeys(OpenQA.Selenium.Keys.LeftControl + "V");
+
+            //// 로그인
+            //naverAccPass.SendKeys(OpenQA.Selenium.Keys.Enter);
+        }
+    }
+}
